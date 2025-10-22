b95dec7031126777a3a8332e0f81f2866d78f608
Arpit Chauhan <arpitchauhan@192.168.1.6>
2025-10-22 19:06:06 +0530

    Intial Commit

---
+++ src/test/java/com/example/Aadhaar_Service/AadhaarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Aadhaar_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AadhaarServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
